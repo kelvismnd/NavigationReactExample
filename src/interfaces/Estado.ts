e13091c28405de3368360310518137e2cb8d6699
kelvismnd <kelvismendes@gmail.com>
2021-12-27 21:46:53 -0300

    modificações

---
+++ src/interfaces/Estado.ts
@@ -0,0 +1,7 @@
+// interface que reflete o retorno de um estado na API
+export interface Estado {
+    id:number;
+    nome:string;
+    sigla:string;
+    regiao:string;
+}
---
+++ src/pages/About.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+export function About(){
+    return (
+        <Text>About Page</Text>
+    );
+}
